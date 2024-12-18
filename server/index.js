cc2fdb0068c7c49002a8f4df653a2b5f2b9c778a
raoufbouk <zakib2316@gmail.com>
2024-12-18 21:15:36 +0100

    backend setup

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3000, () => {
+  console.log("server is running at port 3000");
+});
