6030d9097ae2e54a94b375ae5b83b076ec46c093
raoufbouk <zakib2316@gmail.com>
2024-12-18 23:46:59 +0100

    frontend setup

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="absolute z-10 h-full w-full bg-black bg-opacity-60"></div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    backgroundImage: {
+      bg: "url('/src/assets/bg.webp')",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
