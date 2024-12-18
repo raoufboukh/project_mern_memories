14f51d9287e205a58e9cb9c7143dc03736eb32ab
raoufbouk <zakib2316@gmail.com>
2024-12-18 21:45:16 +0100

    db setup

--- server/index.js
+++ server/index.js
@@ -2,11 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
+const db =
+  "mongodb+srv://raoufbouk:raoufking7@cluster0.0q4iq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 const app = express();
 
 app.use(express.json());
+app.use("/", (req, res) => {
+  res.send("Hello World");
+});
 app.use(cors());
 
-app.listen(3000, () => {
-  console.log("server is running at port 3000");
+mongoose.connect(db).then(() => {
+  console.log("db connected");
+  app.listen(3000, () => {
+    console.log("server is running at port 3000");
+  });
 });
