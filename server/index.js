7331b97e39fc9fcf30e67396e8897466342b9a12
raoufbouk <zakib2316@gmail.com>
2024-12-23 11:45:51 +0100

    make it better

--- server/index.js
+++ server/index.js
@@ -2,10 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import router from "./routers/router.js";
-
-const db =
-  "mongodb+srv://raoufbouk:raoufking7@cluster0.0q4iq.mongodb.net/memories?retryWrites=true&w=majority&appName=Cluster0";
-
 const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
@@ -14,10 +10,12 @@ app.use(
     origin: "http://localhost:5173",
   })
 );
+
+PORT = process.env.PORT || 3000;
 app.use("/", router);
-mongoose.connect(db).then(() => {
+mongoose.connect(process.env.CONNECTION_URL).then(() => {
   console.log("db connected");
-  app.listen(3000, () => {
+  app.listen(PORT, () => {
     console.log("server is running at port 3000");
   });
 });
