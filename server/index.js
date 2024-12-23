fa11b90e16197fde3aeddf453b58883ebd8a5806
raoufbouk <zakib2316@gmail.com>
2024-12-24 00:56:27 +0100

    add vercel.json file

--- server/index.js
+++ server/index.js
@@ -6,11 +6,7 @@ import dotenv from "dotenv";
 const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+app.use(cors());
 
 dotenv.config();
 
