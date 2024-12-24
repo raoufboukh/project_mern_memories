3311b290318f39cc1c0b8a629430b9cf3fbae683
raoufbouk <zakib2316@gmail.com>
2024-12-24 01:19:16 +0100

    ....

--- server/index.js
+++ server/index.js
@@ -3,18 +3,18 @@ import mongoose from "mongoose";
 import cors from "cors";
 import router from "./routers/router.js";
 import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(cors());
-
-dotenv.config();
-
-const PORT = process.env.PORT || 3000;
 app.use("/", router);
-mongoose.connect(process.env.CONNECTION_URL).then(() => {
-  console.log("db connected");
-  app.listen(PORT, () => {
-    console.log("server is running at port 3000");
-  });
-});
+
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() => console.log("DB connected"))
+  .catch((error) => console.error("DB connection error:", error.message));
+
+export default app;
