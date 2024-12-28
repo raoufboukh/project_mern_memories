c6e0945e5c4937b5b76269ebebebf1e77675011b
raoufbouk <zakib2316@gmail.com>
2024-12-28 12:12:23 +0100

    backend modified

--- server/index.js
+++ server/index.js
@@ -9,12 +9,23 @@ dotenv.config();
 const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://project-mern-memories-orpin.vercel.app/",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 app.use("/", router);
+
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.CONNECTION_URL)
-  .then(() => console.log("DB connected"))
+  .then(() => {
+    console.log("DB connected");
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
   .catch((error) => console.error("DB connection error:", error.message));
 
 export default app;
